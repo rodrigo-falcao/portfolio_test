7a592e3b3d1434992b9b74ac4a26ed4cc033b744
Rodrigo Falcão <125101340+rodrigo-falcao@users.noreply.github.com>
2023-07-25 23:22:00 -0300

    alteração do texto e atualizações.

--- src/components/section/presentation/presentation.jsx
+++ src/components/section/presentation/presentation.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 
 const Presentation = () => {
     const [text, setText] = useState ('');
-    const toRotate = ["Juan Reis!", "Dev Front End", "Product Manager"];
+    const toRotate = ["Juan Reis!", "Dev Front End", "Louco por tec!"];
     const [loop, setLoop] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const period = 80;
